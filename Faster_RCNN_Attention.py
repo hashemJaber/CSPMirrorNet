db4dcede26c77472ddc42d0359eebb8715018c5f
hashemJaber <hashemjaber02@gmail.com>
2024-11-05 19:37:48 -0800

    Adding loop Logic, Part 1 - Adding DenseNet loop loose logic - Adding loop documentation soon

--- Faster_RCNN_Attention.py
+++ Faster_RCNN_Attention.py
@@ -7,6 +7,7 @@ import numpy as np
 
 
 
+#NOTE TO SELF: USE SPARSE CONVOLUITONS IN EARLIER LAYERS ?????
 class InputLayer(nn.Module):
    def __init__(self,target_height,target_width)->None:
       super(InputLayer,self).__init__()
@@ -44,9 +45,9 @@ class ConvBlockCSP(nn.Module):
       kernet_size: tuple (w,h), default: (1,1)
       stride:int, effect the (W-kenrnel_size[0]+2*padding)/stride
    output:
-         original feature map, new feature map 
+         original feature map, new feature map
    """
-   def __init__(self, input_channel: int,out_put_channel:int,stride:int=2, kernel_size=(5,5))->None:
+   def __init__(self, input_channel: int,out_put_channel:int,stride:int=2, kernel_size:tuple=(5,5))->None:
        super(ConvBlockCSP,self).__init__()
        self.conv1= nn.Conv2d(input_channel,out_put_channel,kernel_size=kernel_size,stride=2,padding=1 )
        self.activation = nn.Mish()
@@ -57,9 +58,6 @@ class ConvBlockCSP(nn.Module):
        self.bn = nn.BatchNorm2d(out_put_channel)
 
        self.bn2 = nn.BatchNorm2d(out_put_channel)
-
-
-       
 
        #TODO: Add batch normalization
 
@@ -78,50 +76,68 @@ class ConvBlockCSP(nn.Module):
 
      x3 = torch.cat([x1,x2+x1],1)
      print('x3 shape ',x3.shape)
-     return (feauture_map,x3 )
+     return (feauture_map, x3 )
 
 #Take Part 1 and Part 2, then switch to the other parts, such as part 2, and part 1 for cspNet
 
 # Idea: We generate a Parameter generating network to automaically solve for hyper parameters, we use it every 100 epochs as a loss, or maybe the lowest of 10 of all the 100 epochs
 class BaseConvBlockCSP(nn.Module):
-  def __init__(self,input_channel: int,out_put_channel:int)->None:
+  """
+  BaseConvBlockCSP:
+   input:
+
+   output:
+  """
+  def __init__(self,input_channel: int,out_put_channel:int,num_blocks:int=3)->None:
 
     super(BaseConvBlockCSP,self).__init__()
     self.ConvBlockCSP= ConvBlockCSP(input_channel,out_put_channel) # In=3, out=8
     self.ConvBlockCSP2= ConvBlockCSP(out_put_channel*2,out_put_channel*4) #in= 8 out_put_channel*2, out=32
-    self.ConvBlockCSP3= ConvBlockCSP(32,out_put_channel*6)#in 32, out= 48
+    self.ConvBlockCSP3= ConvBlockCSP(out_put_channel*4,out_put_channel*6)#in 32, out= 48
+
 
     self.channel_adjust = nn.Conv2d(3, out_put_channel*6, kernel_size=1, stride=1)
 
         #TODO, Replace with a forloop initialization
 
+    self.blocks = nn.ModuleList()
+
+
+    for i in range(num_blocks):
+
+            in_channels = input_channel if i == 0 else out_put_channel * (2 * i)
+            out_channels = out_put_channel * (2 * (i + 1))
+
+
+            self.blocks.append(ConvBlockCSP(in_channels, out_put_channel))
+
+
+
   def forward(self, base_feature_map:Tensor, use_single_layer:bool=True)->Tensor:
     x1=self.ConvBlockCSP(base_feature_map)
 
     if(use_single_layer):
-          
+
         return x1
+
     x1_part1,x1_part2=x1
     print('x1 part 1',x1_part1.shape)
     print('x1 part 2',x1_part2.shape)
-    x2_part1,x2_part2=self.ConvBlockCSP2(x1_part2) 
+    x2_part1,x2_part2=self.ConvBlockCSP2(x1_part2)
 
     print('x2 part 1',x2_part1.shape)
-    print('x2 part 2',x2_part2.shape) 
+    print('x2 part 2',x2_part2.shape)
 
     x3_part1, x3_part2=self.ConvBlockCSP3(x2_part2)
     print('x3 part 1',x3_part1.shape)
-    print('x3 part 2',x3_part2.shape) 
+    print('x3 part 2',x3_part2.shape)
     x1_part1=self.channel_adjust(x1_part1)
     print('x1_part1 new shape',x1_part1.shape)
-    
+
     x1_part1 = nn.functional.adaptive_avg_pool2d(x1_part1, (x3_part2.size(2), x3_part2.size(3)))
 
 
     return (x3_part1, torch.cat([x1_part1,x3_part2],1))
-
-
-
 
 
 class CSPDarkNet(nn.Module):
@@ -152,7 +168,7 @@ class CSPDarkNet(nn.Module):
 
       height_split = feature_map.shape[2] // 2
       width_split = feature_map.shape[3] // 2
-      overlap_percentage=0.20
+      overlap_percentage=0.20 #could be changed
 
 
       height_overlap = int(height_split * overlap_percentage)
@@ -171,7 +187,7 @@ class CSPDarkNet(nn.Module):
       processed_example2 = self.base_block(example2)[1]
       print("processed_example1", processed_example1.shape)
 
-      # Resuing the same logic we did earlier, we are mimicking a CSPNet Part1, Part2 except cross sectioned
+      # Reusing the same logic we did earlier, we are mimicking a CSPNet Part1, Part2 except that we are adding cross sectioned, think of it like a siamese network
 
       example2 = nn.functional.adaptive_avg_pool2d(example2, (processed_example1.size(2), processed_example1.size(3)))
       example1 = nn.functional.adaptive_avg_pool2d(example1, (processed_example1.size(2), processed_example1.size(3)))
@@ -180,7 +196,7 @@ class CSPDarkNet(nn.Module):
 
       concat2 = torch.cat([example1, processed_example2], dim=1)
 
-      # Sum the concatenated outputs
+      # Sum the concatenated outputs, we can also opt out to concatinate them but this will increase computational cost
       combined_output = concat1 + concat2
 
       print("Shape of example1 (untouched):", example1.shape)
@@ -195,19 +211,17 @@ class CSPDarkNet(nn.Module):
       return combined_output
 
 
-
 class FPN(nn.Module):
     """
     Feature Pyramid Network:
     input:
-        Feature map: output from the backbone
+        Feature map: list, outputs from the backbone
 
     output:
           P1,P2,P3 (each P is a payramid)
     """
     def __init__(self, in_channels: list, out_channels: int):
         super(FPN, self).__init__()
-
 
         self.conv1 = nn.Conv2d(in_channels[0], out_channels, kernel_size=1, stride=1)
         self.conv2 = nn.Conv2d(in_channels[1], out_channels, kernel_size=1, stride=1)
@@ -217,14 +231,15 @@ class FPN(nn.Module):
         self.upconv1 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1) #maybe
 
     def forward(self, feature_map: list) -> tuple:
-        
-        C2, C3, C4 = feature_map 
+
+        C2, C3, C4 = feature_map
 
         P3 = self.conv3(C4)
-        P2 = self.conv2(C3) + F.interpolate(P3, scale_factor=2, mode='nearest')
-        P1 = self.conv1(C2) + F.interpolate(P2, scale_factor=2, mode='nearest')
+        P2 = self.conv2(C3) + F.interpolate(P3, scale_factor=2, mode='nearest') #Use Bilinear maybe ?
+        P1 = self.conv1(C2) + F.interpolate(P2, scale_factor=2, mode='nearest') #Use Bilinear maybe ?
 
         return P1, P2, P3
+
 
 
 class PAN(nn.Module):
@@ -235,11 +250,14 @@ class PAN(nn.Module):
   def __init__(self):
     super(PAN,self).__init__()
     self.conv1= nn.Conv2d(out_put_channel,out_put_channel,kernel_size=kernel_size,stride=1 )
-  def forward()->Tensor:
+  def forward(self, P: list)->Tensor:
+    P1 , P2, P3 = P
+
     return None
 
 
 class APANFPN():
+  #We can use permute to reeshape the final layers then padding so we can conduct
   "Attention Path Aggregation Network Feature Pyramid Netork"
   def _init__(self):
     super(APANFPN,self).__init__()
